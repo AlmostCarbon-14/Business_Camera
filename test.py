e04e35881bb77bf0db30a57673ce053b17600605
Conor Moore <cmoore30@umd.edu>
2020-05-31 21:41:49 -0400

    update

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ import threading
 running = True
 pic_index = 0
 event = threading.Event()
-path = ""
+i_path = ""
 session = 1
 
 def parse_delay():
@@ -37,46 +37,48 @@ def pad(value):
         return "0" + str(value)
     return str(value)
 
-def p_script(lock, path):
-    global pic_index
+def p_script(lock, m_path):
+    global pic_index 
     lock.acquire()
     file_name = "pic-" + pad(pic_index) + ".jpg"
     pic_index += 1
     lock.release()
     try:
         os.system("fswebcam -r 1280x720 " + file_name)
-        os.system("cp pic* " + path)
+        os.system("cp pic* " + m_path)
         os.system("rm pic*")
+        total = int(img_total.cget("text"))
+        print("Total", total)
+        total += 1
+        img_total.config(text= str(total))
+        window.update()
     except:
+        print("EXCEPTION OCCURED")
         lock.acquire()
         pic_index -= 1
         lock.release()
         return
-    total = int(img_total.cget("text"))
-    total += 1
-    img_total.config(text= str(total))
-    window.update()
 
-def take_photo(path):
+def take_photo(m_path):
     lock = threading.Lock()
-    pic_thread = threading.Thread(target = p_script, args = (lock, path ))
+    pic_thread = threading.Thread(target = p_script, args = (lock, m_path ))
     pic_thread.start()
     pic_thread.join()
 
 def system_loop(delay):
-    global path, running, pic_index
-    if path == "":
-        path = build_path()
+    global i_path, running, pic_index
+    if i_path == "":
+        i_path = build_path()
     delay = parse_delay()
     while running:
-        take_photo(path)
+        take_photo(i_path)
         event.wait(timeout= delay)
 
 
 def build_path():
     day = datetime.datetime(date.today().year, date.today().month,  date.today().day)
     day = day.strftime("%d-%b-%Y")
-    img_path = "../../../media/usb/Sessions/" + day + "-" + str(session)
+    img_path = "../../../../media/usb/Sessions/" + day + "-" + str(session)
     if path.exists(img_path) == False:
         os.system("mkdir " + img_path)
     return img_path
@@ -92,47 +94,48 @@ def end_session(ignore):
     event.set()
     session += 1
     build_path = ""
-    result = messagebox.asyesno("Finish", "Would you like to save and shutdown?")
-    print("RESULT:", result)
+    if messagebox.askyesno("Finish", "Would you like to save and shutdown?"):
+        sys.os("sudo shutdown -h now")
     return
 
 
 def force(ignore):
-    global path
-    if path == "":
-        path = build_path()
-    take_photo(path)
+    global i_path
+    if i_path == "":
+        i_path = build_path()
+    take_photo(i_path)
     return
 
 def decrement(ignore):
     val = parse_interval()
     interval.delete(0, 'end')
     if val[0] == "15" and val[1] == "Minutes":
+        interval.insert(0, "15 Minutes")
         return
     if val[1] == "Minutes":
-        interval.set(0, str(int(val[0]) - 15) + " Minutes")
+        interval.insert(0, str(int(val[0]) - 15) + " Minutes")
         return
     if val[0] == "1":
-        interval.set(0, "45 Minutes")
+        interval.insert(0, "45 Minutes")
         return
     if val[0] == "2":
-        interval.set(0, "1 Hour")
+        interval.insert(0, "1 Hour")
     else:
-        interval.set(0, str(int(val[0]) - 1) + " Hours") 
+        interval.insert(0, str(int(val[0]) - 1) + " Hours") 
 
 def increment(ignore):
     val = parse_interval()
     interval.delete(0, 'end')
-    if val[0] == 45 and val[1] == "Minutes":
-        interval.set(0, "1 Hour")
+    if val[0] == "45" and val[1] == "Minutes":
+        interval.insert(0, "1 Hour")
         return
     if val[1] == "Minutes":
-        interval.set(0, str(int(val[0]) + 15) + " Minutes")
+        interval.insert(0, str(int(val[0]) + 15) + " Minutes")
         return
     if val[0] == "1":
-        interval.set(0, "2 Hours")
+        interval.insert(0, "2 Hours")
         return
-    interval.set(0, str(int(val[0]) + 1) + " Hours")
+    interval.insert(0, str(int(val[0]) + 1) + " Hours")
 
 
 window = tk.Tk()
@@ -153,13 +156,13 @@ end_now = tk.Frame(master= window)
 
 title.grid(row= 1, column= 3)
 interval_title.grid(row= 3, column= 3)
-total_pics.grid(row= 6, column= 1)
-total_taken.grid(row= 6, column= 3)
+total_pics.grid(row= 7, column= 1)
+total_taken.grid(row= 7, column= 3)
 
 interval_box.grid(row= 4, column= 3)
 
 interval_dec.grid(row= 4, column= 1)
-interval_dec.grid(row= 4, column= 5)
+interval_inc.grid(row= 4, column= 6)
 take_now.grid(row= 5, column = 3)
 end_now.grid(row= 6, column = 3)
 
@@ -172,8 +175,8 @@ img_total_label = tk.Label(master= total_pics, text= "Total Images Taken", foreg
 interval = tk.Entry(master= interval_box, justify= "center")
 interval.insert(0, "1 Hour")
 
-dec = tk.Button(master= interval_dec, text= "Faster", fg= "red")
-inc = tk.Button(master= interval_inc, text= "Slower", fg= "red")
+inc = tk.Button(master= interval_inc, text= "Faster", fg= "red")
+dec = tk.Button(master= interval_dec, text= "Slower", fg= "red")
 take = tk.Button(master= take_now, text= "Take Photo Now", fg= "red")
 end = tk.Button(master= end_now, text= "End Session", fg= "red")
 
